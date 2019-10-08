7ef96149fc7d5afaa1fd3f453b58d9bb568ee610
Maxime <maxime@vaugam.com>
2019-10-08 17:20:25 +0200

    update

--- scripts/updateNpmVersion.sh
+++ scripts/updateNpmVersion.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git pull develop
-npm version minor
-git add package.json
-git commit -m "bump npm version"
-git push origin develop
+#git pull develop
+#npm version minor
+#git add package.json
+#git commit -m "bump npm version"
+#git push origin develop
+echo "executing script and deploying new version"
