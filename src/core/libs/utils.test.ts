27d9d81883009a2a6bbd711ef3a1fc3f29a7ac31
Stefdelec <s.delecroix@gmail.com>
2019-10-10 11:52:32 +0200

    fix(urlParsed): stop using URL and use parsedURL polyfill

--- src/core/libs/utils.test.ts
+++ src/core/libs/utils.test.ts
@@ -29,7 +29,7 @@ describe("UTILS", () => {
       const tokenId = 1314;
       const passphrase = 'mypassaezfkzn';
       const linkObject = utils.createLink(tokenId, passphrase);
-      
+
       expect(linkObject.tokenId).toBe(tokenId);
       expect(linkObject.passphrase).toBe(passphrase);
 
@@ -70,4 +70,34 @@ describe("UTILS", () => {
     })
   })
 
+  describe("urlParse", () => {
+    const utils = new Utils(undefined, undefined);
+    test('it should parse complicated url', () => {
+      const myURL="http://username:password@localhost:257/deploy/?asd=asd#asd";
+
+      const url=new URL(myURL);
+      const parsedURL = utils.simplifiedParsedURL(myURL);
+
+      expect(parsedURL.hostname).toBe(url.hostname);
+      expect(parsedURL.hash).toBe(url.hash);
+      expect(parsedURL.pathname).toBe(url.pathname);
+      expect(parsedURL.port).toBe(url.port);
+      expect(parsedURL.protocol).toBe(url.protocol);
+      expect(parsedURL.search).toBe(url.search);
+    })
+
+    test('it should parse classic arianee url', () => {
+      
+      const myURL="https://test.arian.ee/722377,ivrsesj4c4nd";
+
+      const parsedURL = utils.simplifiedParsedURL(myURL);
+      const url=new URL(myURL);
+
+      expect(parsedURL.hostname).toBe(url.hostname);
+      expect(parsedURL.pathname).toBe(url.pathname);
+      expect(parsedURL.protocol).toBe(url.protocol);
+    })
+
+
+  })
 })
