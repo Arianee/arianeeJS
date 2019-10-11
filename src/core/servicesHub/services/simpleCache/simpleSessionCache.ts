db66c883c9aa78a71e9b06e0c757239f395faad5
Stefdelec <s.delecroix@gmail.com>
2019-10-11 11:54:17 +0200

    feat(fetch): implement cache manager

---
+++ src/core/servicesHub/services/simpleCache/simpleSessionCache.ts
@@ -0,0 +1,17 @@
+export class SimpleSessionCache {
+    private cache = {};
+
+    get = (key: string) => {
+        if (!this.cache.hasOwnProperty(key)) {
+            return Promise.reject('key');
+        } else {
+            return Promise.resolve(JSON.parse(this.cache[key]));
+        }
+    }
+
+    set = (key: string, value: any) => {
+        this.cache[key] = JSON.stringify(value);
+
+        return Promise.resolve(this);
+    }
+}
