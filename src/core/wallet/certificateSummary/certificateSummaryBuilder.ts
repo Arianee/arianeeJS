b28152c7ee4259d20a480fe7ff809a5ef593cb50
stefdelec <stephane@arianee.org>
2019-10-30 10:01:56 +0100

    feat(certificate): certificateSummary has cerficiatedId property

--- src/core/wallet/certificateSummary/certificateSummaryBuilder.ts
+++ src/core/wallet/certificateSummary/certificateSummaryBuilder.ts
@@ -1,111 +1,115 @@
-import { thisExpression } from "@babel/types";
-import { isNullOrUndefined } from "util";
-import { sortEvents } from "../../libs/sortEvents";
+import {isNullOrUndefined} from "util";
 import {WalletService} from "../services/walletService/walletService";
 import {
-  CertificateAdvanced,
-  CertificateContent,
-  CertificateEvents,
-  CertificateEventsSummary,
-  CertificateIssuer,
-  CertificateOwner,
-  CertificateSummary,
-  CertificiateContent
+    CertificateAdvanced,
+    CertificateContent,
+    CertificateEvents,
+    CertificateEventsSummary,
+    CertificateIssuer,
+    CertificateOwner,
+    CertificateSummary,
+    CertificiateContent
 } from "./certificateSummary";
 
 export class CertificateSummaryBuilder {
-  private _content: CertificiateContent;
-  private _events: CertificateEvents;
-  private _issuer: CertificateIssuer;
-  private _isRequestable: boolean;
-  private _owner: CertificateOwner;
-  private _advanced: CertificateAdvanced;
+    private _content: CertificiateContent;
+    private _events: CertificateEvents;
+    private _issuer: CertificateIssuer;
+    private _isRequestable: boolean;
+    private _owner: CertificateOwner;
+    private _advanced: CertificateAdvanced;
+    private _certificateId: string;
 
-  constructor (private wallet: WalletService) {}
+    public setContent(
+        data: CertificateContent,
+        isAuthentic: boolean
+    ): CertificateSummaryBuilder {
+        this._content = {
+            data,
+            isAuthentic
+        };
 
-  public setContent (
-    data: CertificateContent,
-    isAuthentic: boolean
-  ): CertificateSummaryBuilder {
-    this._content = {
-      data,
-      isAuthentic
-    };
-
-    return this;
-  }
-
-  public setArianeeEvents (events: any[]): CertificateSummaryBuilder {
-    if (isNullOrUndefined(this._events)) {
-      this._events = new CertificateEventsSummary();
+        return this;
     }
-    this._events.arianeeEvents = events;
 
-    return this;
-  }
+    public setCertificateId(certificateId: string): CertificateSummaryBuilder {
+        this._certificateId = certificateId;
 
-  public setEvents (events: any[]): CertificateSummaryBuilder {
-    if (isNullOrUndefined(this._events)) {
-      this._events = new CertificateEventsSummary();
+        return this;
     }
-    this._events.transfert = events;
 
-    return this;
-  }
+    public setArianeeEvents(events: any[]): CertificateSummaryBuilder {
+        if (isNullOrUndefined(this._events)) {
+            this._events = new CertificateEventsSummary();
+        }
+        this._events.arianeeEvents = events;
 
-  public setIsRequestable (isRequestable): CertificateSummaryBuilder {
-    this._isRequestable = isRequestable;
+        return this;
+    }
 
-    return this;
-  }
+    public setEvents(events: any[]): CertificateSummaryBuilder {
+        if (isNullOrUndefined(this._events)) {
+            this._events = new CertificateEventsSummary();
+        }
+        this._events.transfert = events;
 
-  public setIssuer (
-    isIdentityAuthentic,
-    isIdentityVerified,
-    identity?
-  ): CertificateSummaryBuilder {
-    this._issuer = {
-      identity,
-      isIdentityAuthentic,
-      isIdentityVerified
-    };
+        return this;
+    }
 
-    return this;
-  }
+    public setIsRequestable(isRequestable): CertificateSummaryBuilder {
+        this._isRequestable = isRequestable;
 
-  public setAdvandced (
-    advanded: CertificateAdvanced
-  ): CertificateSummaryBuilder {
-    this._advanced = advanded;
+        return this;
+    }
 
-    return this;
-  }
+    public setIssuer(
+        isIdentityAuthentic,
+        isIdentityVerified,
+        identity?
+    ): CertificateSummaryBuilder {
+        this._issuer = {
+            identity,
+            isIdentityAuthentic,
+            isIdentityVerified
+        };
 
-  public setOwner (ownerPublicKey: string) {
-    this._owner = {
-      publicKey: ownerPublicKey,
-      isOwner: ownerPublicKey === this.wallet.publicKey
-    };
+        return this;
+    }
 
-    return this;
-  }
+    public setAdvandced(
+        advanded: CertificateAdvanced
+    ): CertificateSummaryBuilder {
+        this._advanced = advanded;
 
-  build (): CertificateSummary {
-    const arianeCertificate: CertificateSummary = {
-      content: this._content,
-      issuer: this._issuer,
-      isRequestable: this._isRequestable,
-      owner: this._owner,
-      events: this._events,
-      advanced: this._advanced
-    };
+        return this;
+    }
 
-    Object.keys(arianeCertificate).forEach(key => {
-      if (isNullOrUndefined(arianeCertificate[key])) {
-        delete arianeCertificate[key];
-      }
-    });
+    public setOwner(ownerPublicKey: string, currentWallet:string) {
+        this._owner = {
+            publicKey: ownerPublicKey,
+            isOwner: ownerPublicKey === currentWallet
+        };
 
-    return arianeCertificate;
-  }
+        return this;
+    }
+
+    build(): CertificateSummary {
+        const arianeCertificate: CertificateSummary = {
+            certificateId: this._certificateId,
+            content: this._content,
+            issuer: this._issuer,
+            isRequestable: this._isRequestable,
+            owner: this._owner,
+            events: this._events,
+            advanced: this._advanced
+        };
+
+        Object.keys(arianeCertificate).forEach(key => {
+            if (isNullOrUndefined(arianeCertificate[key])) {
+                delete arianeCertificate[key];
+            }
+        });
+
+        return arianeCertificate;
+    }
 }
