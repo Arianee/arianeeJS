c4a903ec23edc17e3b9cedfcab54453dd874ac7f
stefdelec <stephane@arianee.org>
2020-04-10 11:40:43 +0200

    fix(i18n): it uses iso-language-picker with fallback on macros languages (zh, fr,...etc)

--- src/core/libs/i18nSchemaLanguageManager/i18nSchemaLanguageManager.ts
+++ src/core/libs/i18nSchemaLanguageManager/i18nSchemaLanguageManager.ts
@@ -1,21 +1,20 @@
 import { ArianeeCertificatei18n } from '../../../models/jsonSchema/certificates/ArianeeProducti18n';
 import { CertificateSummary } from '../../wallet/certificateSummary';
+import { pickLanguageAccordingToUserLanguages } from 'iso-language-picker';
 
 export const replaceLanguage = (certificateSummary :CertificateSummary<ArianeeCertificatei18n, any>, languages:string[]):CertificateSummary<ArianeeCertificatei18n, any> => {
-  for (let i = 0; i < languages.length; i++) {
-    if (isLanguageExist(certificateSummary.content.data, languages[i])) {
-      certificateSummary.content.data = replaceLanguageContent(certificateSummary.content.data, languages[i]);
-      break;
-    }
+  const availableLanguages = this.availableLanguages(certificateSummary.content.data);
+  const language = pickLanguageAccordingToUserLanguages(languages, availableLanguages);
+
+  if (language) {
+    certificateSummary.content.data = replaceLanguageContent(certificateSummary.content.data, language);
   }
 
   return certificateSummary;
 };
 
-export const isLanguageExist = (certificateId18n :ArianeeCertificatei18n, language:string):boolean => {
-  const translated = certificateId18n.i18n.find(i => i.language === language);
-
-  return translated !== undefined;
+export const availableLanguages = (certificateId18n: ArianeeCertificatei18n, language: string): string[] => {
+  return certificateId18n.i18n.map(i => i.language);
 };
 
 export const replaceLanguageContent = (certificateId18n :ArianeeCertificatei18n, language:string):ArianeeCertificatei18n => {
