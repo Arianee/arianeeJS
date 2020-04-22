f2fdc20827b1fd8bcdc20fcfd1925d91cac8718b
Maxime <maxime@vaugam.com>
2020-04-22 13:37:20 +0200

    feat(identity): Can now get identity from shortId

--- example/myexample.ts
+++ example/myexample.ts
@@ -3,18 +3,9 @@ import { Arianee, NETWORK } from '../src';
 import { blockchainEventsName } from '../src/models/blockchainEventsName';
 
 (async function () {
-  const arianee = await new Arianee().init(NETWORK.arianeeTestnet);
+  const arianee = await new Arianee().init(NETWORK.testnet);
 
   const wallet = arianee.fromRandomKey();
-
-  await wallet.methods.approveStore();
-
-  wallet.methods.createCertificate({
-    content: {
-      $schema: 'https://cert.arianee.org/version1/ArianeeAsset.json',
-      name: 'Arianee'
-    }
-  })
-    .then(i => console.log('sucess', i))
-    .catch(d => console.log('errror', d));
+  const identity = await wallet.methods.getIdentityByShortId('0x135935');
+  console.log(identity);
 })();
