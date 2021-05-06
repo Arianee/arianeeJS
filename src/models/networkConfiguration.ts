e1b1c66725fbf58f8accc8904aad33b2768eece3
stefdelec <stephane@arianee.org>
2021-05-06 11:02:07 +0200

    feat(network): add mumbai and matic polygon networks

--- src/models/networkConfiguration.ts
+++ src/models/networkConfiguration.ts
@@ -1,10 +1,14 @@
 export enum NETWORK {
   testnet = 'testnet',
   mainnet = 'mainnet',
-  arianeeTestnet = 'arianeetestnet'
+  arianeeTestnet = 'arianeetestnet',
+  mumbai='mumbai',
+  polygon='polygon'
 }
 
 export const networkURL = {
+  [NETWORK.polygon]: 'https://cert.arianee.net/contractAddresses/polygon.json',
+  [NETWORK.mumbai]: 'https://cert.arianee.net/contractAddresses/mumbai.json',
   [NETWORK.testnet]: 'https://cert.arianee.net/contractAddresses/newsokol.json',
   [NETWORK.mainnet]:
     'https://cert.arianee.net/contractAddresses/newpoacore.json',
